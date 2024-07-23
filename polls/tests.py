99dbf0d0bf1ef717aa1cb164bfc8bb7ea39c1270
Nick <nickbelik@mail.ru>
2024-07-23 11:35:43 +0300

    template test

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from celery import shared_task
+from .tasks import add
+
+class CeleryTasksTest(TestCase):
+
+    def test_add_task(self):
+        # Run the task directly
+        result = add(4, 6)
+        self.assertEqual(result, 10)
+
+        # Alternatively, you can apply the task to test it asynchronously
+        result = add.apply(args=[4, 6]).get()
+        self.assertEqual(result, 10)
